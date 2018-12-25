26d3f2bfe1cda4426f1c4752c63098cdd6f192ef
SSnowHostBDream <46120374+SSnowHostBDream@users.noreply.github.com>
2018-12-25 08:28:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "."
-var adminprefix = '.'
+var prefix = "#"
+var adminprefix = '#'
 
 client.on("message", (message) => {
     /// ALPHA CODES
@@ -93,15 +93,5 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 }
 });
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`.help | Server ${client.guilds.size}`,"http://twitch.tv/Dream")
-client.user.setStatus("dnd")
-});
 
 client.login(process.env.TOKEN);
